a92aef1ab0bed9abfdc80dc09ede7f13afeedba1
gaeun0617 <dl85097@gmail.com>
2019-07-03 18:34:00 +0900

    init

--- justwindow.py
+++ justwindow.py
@@ -9,14 +9,13 @@ pygame.display.set_caption("Just Window")
 def main():
     """main routine"""
     while True:
-        SURFACE.fill((255,255,255))
+        SURFACE.fill((0,255,255))
         
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-    
-    pygame.display.update()
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
